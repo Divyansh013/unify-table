f14e777a04c161953f8f7500bae1a0c973bc2b92
Divyansh Srivastava <divyansh.srivastava.cd.che20@itbhu.ac.in>
2024-07-06 13:10:16 +0530

    used json to dynamically fetch data

--- main.js
+++ main.js
@@ -14,3 +14,37 @@ document.addEventListener('DOMContentLoaded', () => {
     loadTemplate('navbar', 'components/navbar/navbar.hbs');
     loadTemplate('table', 'components/table/table.hbs');
 });
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('data.json')
+      .then(response => response.json())
+      .then(jsonData => {
+        const table = document.getElementById('data-table');
+        table.querySelectorAll('tr').forEach((row, rowIndex) => {
+          const data = jsonData[rowIndex];
+          const cells = row.querySelectorAll('td');
+          cells[1].querySelector('div').textContent = data.automation_name;
+          cells[2].querySelector('div>span').textContent = data.trigger;
+          
+          const images = cells[3].querySelector('.imgcontainer');
+          images.innerHTML = '';
+
+    data.applications.forEach(application => {
+        const img = document.createElement('img');
+        img.src = application; 
+        img.width = 30; 
+        img.height = 30; 
+        img.alt = `${application} icon`; 
+        img.classList.add('application-icon'); 
+        images.appendChild(img);
+    });
+
+          cells[5].querySelector('.flex .flex-col > div:nth-child(1)').textContent=data.created_by.name;
+          cells[5].querySelector('.flex .flex-col > div:nth-child(2)').textContent=data.created_by.time;
+          cells[6].querySelector('.flex .flex-col > div:nth-child(1)').textContent=data.last_modified_by.name;
+          cells[6].querySelector('.flex .flex-col > div:nth-child(2)').textContent=data.last_modified_by.time;
+        });
+      })
+      .catch(error => console.error('Error loading JSON:', error));
+  });
